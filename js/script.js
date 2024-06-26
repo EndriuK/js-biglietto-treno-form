218c4d0d0cbfa693b37444580a97f45f16fd0a0f
EndriuK <157508097+EndriuK@users.noreply.github.com>
2024-06-26 15:56:01 +0200

    [UPD] aggiunta condizione per età < 18

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ let km = prompt("Inserisci i km da percorrere")
 let eta = prompt("Inserisci la tua età")
 
 let prezzoBase = km * 0.21
+
+let prezzFinale; 
+
+if (eta < 18) {
+    prezzoFinale = prezzoBase * 0.8
+}
+
+console.log(prezzoFinale)
